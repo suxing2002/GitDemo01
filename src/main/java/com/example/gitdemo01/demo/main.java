e69a47acc888e7db5bac39c6e9cbd43d6894eeda
guoshuo <2056553678@qq.com>
2022-10-09 18:20:23 +0800

    first commit

---
+++ src/main/java/com/example/gitdemo01/demo/main.java
@@ -0,0 +1,13 @@
+package com.example.gitdemo01.demo;
+
+/**
+ * @Author GS
+ * @Time 2022/10/9 18:07
+ * @Version 1.0
+ * @Description
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
